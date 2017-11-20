7a5c21e2f97e71ef62d2bf3b6ba659d3e3a2f6af
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-11-20 12:37:52 +0100

    Inicio proyecto

---
+++ boletin9_2/src/boletin9_2/Boletin9_2.java
@@ -0,0 +1,14 @@
+
+package boletin9_2;
+
+
+public class Boletin9_2 {
+
+    
+    public static void main(String[] args) {
+        
+        Clase1 obj=new Clase1();
+        obj.calcular();
+    }
+    
+}
---
+++ boletin9_2/src/boletin9_2/Clase1.java
@@ -0,0 +1,21 @@
+
+package boletin9_2;
+
+
+public class Clase1 {
+    
+public void calcular(){
+    
+double sum=0, prod=1, i=10;
+    
+     
+    while (i < 90) {
+    sum = sum + i;
+    prod=prod*i;
+    ++i;
+     }
+    
+    System.out.println("suma: "+sum+"  producto: " +prod);
+    
+   }   
+}
